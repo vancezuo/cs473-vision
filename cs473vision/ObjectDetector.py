fd873347091c7a0c06328d46711430eaade7f5b8
orkinus <vance.zuo@gmail.com>
2014-03-11 22:53:23 -0400

    Add additional test cases.
    New test cases are located in example2 folder. ObjectDetector.py
    rewritten to reflect the change.

--- cs473vision/ObjectDetector.py
+++ cs473vision/ObjectDetector.py
@@ -28,10 +28,10 @@ class ObjectDetector(object):
         self.fg_img = cv2.imread(fg_path)
         return True
         
-    def create_fg_mask(self, method="Simple"):
+    def create_fg_mask(self, method="simple"):
         if (self.bg_img is None) or (self.fg_img is None):
             return False
-        if method == "Simple":
+        if method == "simple":
             self.fg_mask = cv2.absdiff(self.bg_img, self.fg_img)
             self.fg_mask = cv2.cvtColor(self.fg_mask, cv2.COLOR_BGR2GRAY)
             __, self.fg_mask = cv2.threshold(self.fg_mask, 0, 255,
@@ -45,7 +45,7 @@ class ObjectDetector(object):
             bg_subtractor.apply(self.bg_img)
             self.fg_mask = bg_subtractor.apply(self.fg_img)
             __, self.fg_mask = cv2.threshold(self.fg_mask, 128, 255, 
-                                         cv2.THRESH_BINARY)
+                                             cv2.THRESH_BINARY)
         else:
             return False
         return True
@@ -59,29 +59,20 @@ class ObjectDetector(object):
         areas = [cv2.contourArea(c) for c in contours]
         return cv2.boundingRect(contours[numpy.argmax(areas)])
     
-
 def main():
-    # Load image
+    tests = [("example1/square.jpg", "example1/arm.jpg", "example1/"),
+             ("example2/bg.jpg", "example2/fg1.jpg", "example2/fg1_"),
+             ("example2/bg.jpg", "example2/fg2.jpg", "example2/fg2_"), ]
     obj = ObjectDetector()
-    obj.load_image("example1/square.jpg", "example1/arm+square.jpg")   
-    # Test simple difference mask
-    obj.create_fg_mask("Simple")  
-    x, y, width, height = obj.get_object_rectangle()
-    cv2.rectangle(obj.fg_mask, (x,y), (x+width,y+height), (128,128,128))
-    cv2.imwrite("example1/simple.png", obj.fg_mask) 
-    print "Simple difference rectangle:", obj.get_object_rectangle()
-    # Test MOG mask
-    obj.create_fg_mask("MOG")  
-    x, y, width, height = obj.get_object_rectangle()
-    cv2.rectangle(obj.fg_mask, (x,y), (x+width,y+height), (128,128,128))
-    cv2.imwrite("example1/MOG.png", obj.fg_mask)
-    print "MOG result rectangle:", obj.get_object_rectangle()
-    # Test MOG2 mask
-    obj.create_fg_mask("MOG2")  
-    x, y, width, height = obj.get_object_rectangle()
-    cv2.rectangle(obj.fg_mask, (x,y), (x+width,y+height), (128,128,128))
-    cv2.imwrite("example1/MOG2.png", obj.fg_mask)
-    print "MOG2 result rectangle:", obj.get_object_rectangle()
+    for test_param in tests:
+        obj.load_image(test_param[0], test_param[1])
+        print "Testing:", test_param
+        for method in ["simple", "MOG", "MOG2"]:
+            obj.create_fg_mask(method)  
+            x, y, width, height = obj.get_object_rectangle()
+            cv2.rectangle(obj.fg_mask, (x,y), (x+width,y+height), (128,128,128))
+            cv2.imwrite(test_param[2] + method + ".png", obj.fg_mask) 
+            print "Rectangle (" + method + "):", obj.get_object_rectangle()   
     return
     
 if __name__ == "__main__":
