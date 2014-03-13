0023a91bb1f55be92fe3c789e4b2a706af588647
Vance Zuo <vance.zuo@gmail.com>
2014-03-12 23:19:03 -0400

    Add coin image test cases.
    The new test case contains 3 foreground images: penny, nickel, and 12
    stacked pennies. They use the same background image.
    
    The ObjectDetector module was also edited to apply a little bilateral
    filtering to the loaded images. This seemed to improve the results.

--- cs473vision/ObjectDetector.py
+++ cs473vision/ObjectDetector.py
@@ -26,6 +26,10 @@ class ObjectDetector(object):
             return False
         self.bg_img = cv2.imread(bg_path)
         self.fg_img = cv2.imread(fg_path)
+        #self.bg_img = cv2.medianBlur(self.bg_img, 9)
+        #self.fg_img = cv2.medianBlur(self.fg_img, 9)
+        self.bg_img = cv2.bilateralFilter(self.bg_img, 5, 100, 100)
+        self.fg_img = cv2.bilateralFilter(self.fg_img, 5, 100, 100)
         return True
         
     def create_fg_mask(self, method="simple"):
@@ -52,17 +56,22 @@ class ObjectDetector(object):
     
     def get_object_rectangle(self):
         if self.fg_mask is None:
-            return (0, 0)
+            return (0, 0, 0, 0) 
         fg_mask_copy = self.fg_mask.copy()
         contours, __ = cv2.findContours(fg_mask_copy, cv2.RETR_TREE,
                                         cv2.CHAIN_APPROX_SIMPLE)
         areas = [cv2.contourArea(c) for c in contours]
+        if not areas:
+            return (0, 0, 0, 0)
         return cv2.boundingRect(contours[numpy.argmax(areas)])
     
 def main():
     tests = [("example1/square.jpg", "example1/arm.jpg", "example1/"),
              ("example2/bg.jpg", "example2/fg1.jpg", "example2/fg1_"),
-             ("example2/bg.jpg", "example2/fg2.jpg", "example2/fg2_"), ]
+             ("example2/bg.jpg", "example2/fg2.jpg", "example2/fg2_"), 
+             ("example3/bg.jpg", "example3/fg-nickel.jpg", "example3/fg-nickel_"),
+             ("example3/bg.jpg", "example3/fg-penny.jpg", "example3/fg-penny_"), 
+             ("example3/bg.jpg", "example3/fg-penny12.jpg", "example3/fg-penny12_")]
     obj = ObjectDetector()
     for test_param in tests:
         obj.load_image(test_param[0], test_param[1])
